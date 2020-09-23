8999af381a14773d0c8d3ae6f6ef0e4828f12562
Alanna Bouloy <lanihope@gmail.com>
2020-09-23 19:43:09 -0400

    api function stubs

---
+++ src/api.js
@@ -0,0 +1,29 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/alanna/bookmarks';
+
+const listApiFetch = function(...args){
+
+};
+
+const getBookmarks = function(){
+
+};
+
+const createBookmark = function(){
+
+};
+
+const updateBookmark = function(){
+
+};
+
+const deleteBookmark = function(){
+
+};
+
+
+export default {
+  getBookmarks,
+  createBookmark,
+  updateBookmark,
+  deleteBookmark
+};
