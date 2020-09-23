4cb8c6179e08538e1b7a7eae06399d5d0eb414e3
Alanna Bouloy <lanihope@gmail.com>
2020-09-23 17:01:14 -0400

    set up store.js

---
+++ src/store.js
@@ -0,0 +1,58 @@
+const bookmarks = [];
+//each bookmark should be and object with following data: title (str), url(str), description(str), rating(num 1-5), expanded(bool), id(str)
+
+let bookmarkNum = 0;
+let adding = false;
+let editing = false;
+let error = null;
+let filter = 0;
+
+
+const addBookmark = function(bookmark){
+
+};
+
+const findAndDelete = function(id){
+
+};
+
+const toggleAdding = function(){
+
+};
+
+const toggleEditing = function(){
+
+};
+
+const changeFilter = function(rating){
+
+};
+
+const findAndUpdate = function(id, newData){
+
+};
+
+const bookmarkCounter = function(){
+
+};
+
+const setError = function(error){
+
+};
+
+export default {
+  bookmarks,
+  bookmarkNum,
+  error,
+  adding,
+  editing,
+  filter,
+  addBookmark,
+  findAndDelete,
+  toggleAdding,
+  toggleEditing,
+  changeFilter,
+  findAndUpdate,
+  bookmarkCounter,
+  setError
+};
